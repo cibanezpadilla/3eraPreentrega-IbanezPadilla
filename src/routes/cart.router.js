09dd35c471ce0ae0aa724277d3bf068903b9248c
Clemencia Ibanez Padilla <clemenciaibanezpadilla@gmail.com>
2023-12-25 21:12:36 -0300

    authMiddleware

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -15,7 +15,7 @@ router.get('/:cid', findCart)
 
 
 /* ADD PRODUCT TO CART */
-router.post('/:cid/products/:pid', /* authMiddleware(["USER"]), */ addProductToCart)
+router.post('/:cid/products/:pid', authMiddleware(["USER"]), addProductToCart)
 
 
 
